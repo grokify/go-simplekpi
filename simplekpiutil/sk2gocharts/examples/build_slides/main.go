4d2afacd3956e0f49a73bb2231feacf252b73f83
John Wang <johncwang@gmail.com>
2020-04-14 03:55:15 -0700

    add slides example

---
+++ simplekpiutil/sk2gocharts/examples/build_slides/main.go
@@ -0,0 +1,146 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/antihax/optional"
+	"github.com/grokify/go-simplekpi/simplekpi"
+	"github.com/grokify/go-simplekpi/simplekpiutil"
+	"github.com/grokify/go-simplekpi/simplekpiutil/sk2gocharts"
+	"github.com/grokify/googleutil/slidesutil/v1"
+	"github.com/grokify/gotilla/config"
+	"github.com/grokify/gotilla/fmt/fmtutil"
+	"github.com/grokify/oauth2more/google"
+	"github.com/jessevdk/go-flags"
+)
+
+type Options struct {
+	EnvPath  string `short:"e" long:"envpath" description:".env Filepath" required:"false"`
+	Site     string `short:"s" long:"site" description:"Your site" required:"false"`
+	Username string `short:"u" long:"username" description:"Your username" required:"false"`
+	Password string `short:"p" long:"password" description:"Your password" required:"false"`
+	Kpiid    int32  `short:"k" long:"kpiid" description:"KPI ID" required:"false"`
+}
+
+func main() {
+	opts := Options{}
+	_, err := flags.Parse(&opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if len(opts.Site) == 0 {
+		_, err := config.LoadDotEnv(".env", os.Getenv("ENV_PATH"), opts.EnvPath)
+		if err != nil {
+			log.Fatal(err)
+		}
+		opts.Site = os.Getenv("SIMPLEKPI_SITE")
+		opts.Username = os.Getenv("SIMPLEKPI_USERNAME")
+		opts.Password = os.Getenv("SIMPLEKPI_TOKEN")
+	}
+
+	skAPIClient, err := simplekpiutil.NewApiClient(opts.Site, opts.Username, opts.Password)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	googHTTPClient, err := google.NewClientFileStoreWithDefaultsCliEnv("", "")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pc, err := slidesutil.NewPresentationCreator(googHTTPClient)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_, err = pc.Create(
+		"Platform Analytics - "+time.Now().Format(time.RFC3339),
+		"Platform Analytics",
+		"Developer Program")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	kpis := []uint64{94, 92, 93}
+
+	for _, kpiID := range kpis {
+		err = sk2gocharts.CreateKPISlide(skAPIClient, pc,
+			kpiID, "https://aae6183a.ngrok.io/",
+			fmt.Sprintf("Source: AGW Logs via\nMetabase &\nSimpleKPI #%d", kpiID),
+			true)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	if 1 == 0 {
+		kpi, resp, err := skAPIClient.KPIsApi.GetKPI(
+			context.Background(),
+			int64(opts.Kpiid))
+		if err != nil {
+			log.Fatal(err)
+		} else if resp.StatusCode > 299 {
+			log.Fatal(resp.StatusCode)
+		}
+		fmtutil.PrintJSON(kpi)
+	}
+	if 1 == 0 {
+		params := &simplekpi.GetAllKPIEntriesOpts{}
+		if opts.Kpiid > 0 {
+			params.Kpiid = optional.NewInt32(opts.Kpiid)
+		}
+
+		kpientries, resp, err := skAPIClient.KPIEntriesApi.GetAllKPIEntries(
+			context.Background(),
+			"2010-01-01",
+			"2020-02-01",
+			params,
+		)
+		if err != nil {
+			log.Fatal(err)
+		} else if resp.StatusCode > 299 {
+			log.Fatal(resp.StatusCode)
+		}
+		fmtutil.PrintJSON(kpientries)
+	}
+	if 1 == 0 {
+		test := simplekpi.KpiEntry{
+			KpiId:     111,
+			UserId:    222,
+			EntryDate: "2020-01-01",
+			Actual:    12345.0,
+		}
+		kpi, resp, err := skAPIClient.KPIEntriesApi.AddKPIEntry(
+			context.Background(),
+			test,
+		)
+		if err != nil {
+			log.Fatal(err)
+		} else if resp.StatusCode > 299 {
+			log.Fatal(resp.StatusCode)
+		}
+		fmt.Printf("SUCCESS [%v]\n", resp.StatusCode)
+		fmtutil.PrintJSON(kpi)
+	}
+
+	if 1 == 0 {
+		kpientryid := int64(333)
+		resp, err := skAPIClient.KPIEntriesApi.DeleteKPIEntry(
+			context.Background(),
+			kpientryid,
+		)
+		if err != nil {
+			log.Fatal(err)
+		} else if resp.StatusCode > 299 {
+			log.Fatal(resp.StatusCode)
+		}
+		fmt.Printf("DELETE SUCCESS [%v]\n", resp.StatusCode)
+	}
+
+	fmt.Println("DONE")
+}
