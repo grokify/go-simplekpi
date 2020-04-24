2da60752dc09d17083fee09e0f653faaef06488c
John Wang <johncwang@gmail.com>
2020-04-23 22:32:59 -0700

    support charts filtering

--- charts/google_slides_set.go
+++ charts/google_slides_set.go
@@ -32,3 +32,28 @@ func CreateKPISlides(skClient *simplekpi.APIClient, pc *slidesutil.PresentationC
 	}
 	return nil
 }
+
+func (set *SlidesInfoSet) Count() int {
+	return len(set.KpiSlideOptsList)
+}
+
+func (set *SlidesInfoSet) Filter(kpiIDs []uint64) SlidesInfoSet {
+	if len(kpiIDs) == 0 {
+		return *set
+	}
+	newSlidesInfoSet := SlidesInfoSet{
+		ImageBaseURL:     set.ImageBaseURL,
+		Verbose:          set.Verbose,
+		KpiSlideOptsList: []KpiSlideOpts{}}
+	mapKpis := map[uint64]int{}
+	for _, kpiID := range kpiIDs {
+		mapKpis[kpiID] = 1
+	}
+	for _, opts := range set.KpiSlideOptsList {
+		if _, ok := mapKpis[opts.KpiID]; ok {
+			newSlidesInfoSet.KpiSlideOptsList = append(
+				newSlidesInfoSet.KpiSlideOptsList, opts)
+		}
+	}
+	return newSlidesInfoSet
+}
